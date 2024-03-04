79c7f772af9b5e1e651a09197c7394d3169e53b1
Sorbopriyo Roy <sorbopriyo@gmail.com>
2024-03-04 22:27:35 +0530

    cors setup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { DATA_LIMIT } from "./constants.js";
 
 const app = express();
+const corsConfig = { origin: process.env.CORS_ORIGIN, credentials: true };
 const jsonConfig = { limit: DATA_LIMIT };
 const urlEncodingConfig = { limit: DATA_LIMIT, extended: true };
 
+app.use(cors(corsConfig));
 app.use(express.json(jsonConfig));
 app.use(express.urlencoded(urlEncodingConfig));
 app.use(express.static("public"));
