44a16cd4b209fba5f7f6095f5507d55e69214cfb
Sorbopriyo Roy <sorbopriyo@gmail.com>
2024-05-25 15:14:01 +0530

    all changes till 25th may

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,21 @@
+// import { asyncHandler } from "../utils/asyncHandler";
+
+// const getVideoComments = asyncHandler(async (req, req) => {
+//   const { videoId } = req.params;
+//   const { page = 1, limit = 10 } = req.query;
+//   // TODO: get all comments for a video
+// });
+
+// const addComment = asyncHandler(async (req, res) => {
+//   // add comment to a video
+// });
+
+// const updateComment = asyncHandler(async (req, res) => {
+//   // TODO: update a comment
+// });
+
+// const deleteComment = asyncHandler(async (req, res) => {
+//   // TODO: delete a comment
+// });
+
+// export { getVideoComments, addComment, updateComment, deleteComment };
