8a8a876ea537fe07cd022e4d7eaaedbc8c591fd8
Sorbopriyo Roy <sorbopriyo@gmail.com>
2024-07-01 20:25:26 +0530

    Newsletter subscription works with logger

--- src/controllers/newsletter.controller.js
+++ src/controllers/newsletter.controller.js
@@ -2,16 +2,20 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Newsletter } from "../models/newsletter.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { getCurrentTimeString } from "../utils/getCurrentTimeString.js";
 
 const newsletter = asyncHandler(async (req, res) => {
   const { email } = req.body;
+  console.log(getCurrentTimeString() + "Request received: Newsletter subscription for " + email);
 
   if (!email || email === "") {
+    console.log(getCurrentTimeString() + "Error: Empty email field provided");
     throw new ApiError(400, "Email field is required");
   }
-
+  
   const existingEmail = await Newsletter.findOne({ email });
   if (existingEmail) {
+    console.log(getCurrentTimeString() + "Error: Email already subscribed");
     throw new ApiError(409, "You have already subscribed to our newsletter");
   }
 
@@ -23,6 +27,7 @@ const newsletter = asyncHandler(async (req, res) => {
     );
   }
 
+  console.log(getCurrentTimeString() + "Response sent: Newsletter subscribed for " + email);
   return res
     .status(200)
     .json(
